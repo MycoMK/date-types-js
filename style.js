ca322d25cc0d0dc13cf3b9ea5569bf489cb8c35b
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-01-09 00:11:13 -0800

    js-data-types

---
+++ style.js
@@ -0,0 +1,13 @@
+alert('connected')
+
+let firstName = 'Red'
+let lastName =  'Sox'
+let city = 'Boston'
+let wins = '250'
+const game = 'Baseball'
+
+let fullName = firstName + ' ' + lastName
+
+let team = ` The ${fullName}, from ${city} have over ${wins} wins in the ${game} world wide.`
+
+Console.log (team)
