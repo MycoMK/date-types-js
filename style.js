c7b3a6b64d94be0b226721eb104906b0956871af
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-01-09 00:14:59 -0800

    Update style.js

--- style.js
+++ style.js
@@ -3,11 +3,11 @@ alert('connected')
 let firstName = 'Red'
 let lastName =  'Sox'
 let city = 'Boston'
-let wins = '250'
+let wins = 250
 const game = 'Baseball'
 
 let fullName = firstName + ' ' + lastName
 
-let team = ` The ${fullName}, from ${city} have over ${wins} wins in the ${game} world wide.`
+let Team = ` The ${fullName}, from ${city} have over ${wins} wins in ${game} world wide.`
 
-Console.log (team)
+Console.log(team)
