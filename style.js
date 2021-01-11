419aef362380e54f4dc656d1df113c74a59f44f8
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-01-11 10:51:21 -0800

    Update style.js

--- style.js
+++ style.js
@@ -8,6 +8,6 @@ const game = 'Baseball'
 
 let fullName = firstName + ' ' + lastName
 
-let Team = ` The ${fullName}, from ${city} have over ${wins} wins in ${game} world wide.`
+let team = ` The ${fullName}, from ${city} have over ${wins} wins in ${game} world wide.`
 
-Console.log(team)
+console.log(team)
